f1e8d76e23233f080fc7f89a29e16a95de040041
Felix Narciso <49419433+felixnarciso@users.noreply.github.com>
2024-03-05 13:32:58 +0000

    Getting through the course!

---
+++ assignments.js
@@ -0,0 +1,37 @@
+/*
+const country = "Argentina";
+const continent = "South America";
+let population = 46000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+const isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+language = 'spanish';
+
+const splitInHalf = population / 2;
+population++;
+console.log(splitInHalf);
+
+let finlandPopulation = 6000000;
+
+console.log(population > finlandPopulation);
+
+let avgPopulation = 33000000;
+
+console.log(population < avgPopulation);
+
+const description = country + ' is in ' + continent + ', and its ' + population + ' people speak ' + language;
+console.log(description);
+
+const descriptionNew = `${country} is in ${continent}, and its ${population} people speak ${language}`;
+console.log(descriptionNew);
+*/
---
+++ script.js
@@ -0,0 +1,96 @@
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("jonas");
+console.log("23");
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+
+console.log(myFirstJob);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3
+
+const firstName = 'Jonas';
+const lastName = 'Narciso';
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah); // <, >, <=, >=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+const firstName = "Jonas";
+const job = "teacher";
+const year = 2037;
+const birthYear = 1991;
+
+const jonas = "I'm " + firstName + ", a " + (year - birthYear) + " year old " + job + "!";
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`
+console.log(jonasNew);
+
+const age = 16;
+
+if (age >= 18) {
+    console.log(`Sarah can start driving license!`);
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`gtfo and come back in ${yearsLeft} years`);
+}
+
+let century;
+const birthYear = 2012;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
+*/
