0bbc27638fa12c31d3adaf0aafe516bae4e9da84
Felix Narciso <49419433+felixnarciso@users.noreply.github.com>
2024-03-08 20:25:40 +0000

    The grindset continues

--- script.js
+++ script.js
@@ -239,3 +239,21 @@ if (day === `monday`)
     console.log(`Not a valid day!`);
 }
 */
+//The conditional operator
+const age = 23;
+//age >= 18 ? console.log(`I like to drink wine`) : console.log(`I like to drink water`);
+
+const drink = age >= 18 ? `wine` : `water`;//More common way to use it
+console.log(drink);
+
+let drink2; //look at how much time we save with the former operator
+if (age >= 18)
+{
+    drink2 = `wine`;
+} else 
+{
+    drink2 = `water`;
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? `wine` : `water`}`)
